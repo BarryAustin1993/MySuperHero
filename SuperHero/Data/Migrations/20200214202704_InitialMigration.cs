b07ee3d9fc21132a4ca5c59b83cba77f474fe465
Austin <ajpbarry@gmail.com>
2020-02-14 15:18:50 -0600

    finished creation steps

---
+++ SuperHero/Data/Migrations/20200214202704_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHero.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeros",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SuperHeroName = table.Column<string>(nullable: true),
+                    AlterEgo = table.Column<string>(nullable: true),
+                    PrimarySuperheroAbility = table.Column<string>(nullable: true),
+                    SecondarySuperHeroAbility = table.Column<string>(nullable: true),
+                    CatchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeros", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeros");
+        }
+    }
+}
---
+++ SuperHero/Models/CSuperHero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero.Models
+{
+    public class CSuperHero
+    {
+
+        //Member Variables (HAS A)
+        [Key]
+        public int ID { get; set; }
+        public string SuperHeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimarySuperheroAbility { get; set; }
+        public string SecondarySuperHeroAbility { get; set; }
+        public string CatchPhrase { get; set; }
+        
+        //Constructor
+
+        
+        //Member Methods (CAN DO)
+
+    }
+}
