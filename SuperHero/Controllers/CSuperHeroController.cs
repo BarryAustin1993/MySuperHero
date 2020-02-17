57222afc748b9905afdc170912de530a1d0f47dd
Austin <ajpbarry@gmail.com>
2020-02-17 08:49:59 -0600

    finished the CRUD operations

--- SuperHero/Controllers/CSuperHeroController.cs
+++ SuperHero/Controllers/CSuperHeroController.cs
@@ -25,86 +25,78 @@ namespace SuperHero.Controllers
         }
 
         // GET: CSuperHero/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int ID)
         {
             return View();
         }
 
         // GET: CSuperHero/Create
-        public ActionResult Create(CSuperHero superhero)
+        public ActionResult Create()
         {
-            if(ModelState.IsValid)
-            {
-                _context.SuperHeros.Add(superhero);
-                _context.SaveChanges();
-                return View();
-            }
-            else
-            {
-                return Create(superhero);
-            }
+            CSuperHero superHero = new CSuperHero();
+            return View(superHero);
         }
 
         // POST: CSuperHero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(CSuperHero superhero)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
+                _context.SuperHeros.Add(superhero);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
+            else
             {
-                return View();
+                return RedirectToAction("Index");
             }
         }
 
         // GET: CSuperHero/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int ID)
         {
-            return View();
+            return View(_context.SuperHeros.Find(ID));
         }
 
         // POST: CSuperHero/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, CSuperHero superHero)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
+                _context.SuperHeros.Update(superHero);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
+            else
             {
-                return View();
+                return Edit(id, superHero);
             }
         }
 
         // GET: CSuperHero/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(_context.SuperHeros.Find(id));
         }
 
         // POST: CSuperHero/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int ID, CSuperHero superHero)
         {
-            try
+            if(ModelState.IsValid)
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
+                _context.SuperHeros.Remove(superHero);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
+            else
             {
-                return View();
+                return Delete(ID, superHero);
             }
         }
     }
